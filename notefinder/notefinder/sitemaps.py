de82fb103d1c5f793f393e35bfe880b39ee778d1
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-05-30 20:21:05 +0545

    Completes deptsem sitemap

--- notefinder/notefinder/sitemaps.py
+++ notefinder/notefinder/sitemaps.py
@@ -3,22 +3,29 @@ from resources.models import Course, ResourceItem, ResourceURL, DeptSem
 
 
 class ResourceItemSitemap(Sitemap):
-    changefreq = "never"
+    changefreq = "daily"
     priority = 0.9
 
     def items(self):
         return ResourceItem.objects.all()
 
 class ResourceURLSitemap(Sitemap):
-    changefreq = "never"
+    changefreq = "daily"
     priority = 0.9
 
     def items(self):
         return ResourceURL.objects.all()
 
 class CourseSitemap(Sitemap):
-    changefreq = "never"
+    changefreq = "daily"
     priority = 0.9
 
     def items(self):
         return Course.objects.all()
+
+class DeptSemSitemap(Sitemap):
+    changefreq = "daily"
+    priority = 0.9
+
+    def items(self):
+        return DeptSem.objects.all()
