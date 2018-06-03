8ab1ee7a54fda626afec254e4b694fd65b2f8f25
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-06-03 09:20:39 +0545

    removes change frequency and prority from the sitemap

--- notefinder/notefinder/sitemaps.py
+++ notefinder/notefinder/sitemaps.py
@@ -3,29 +3,24 @@ from resources.models import Course, ResourceItem, ResourceURL, DeptSem
 
 
 class ResourceItemSitemap(Sitemap):
-    changefreq = "daily"
-    priority = 0.9
+
 
     def items(self):
         return ResourceItem.objects.all()
 
 class ResourceURLSitemap(Sitemap):
-    changefreq = "daily"
-    priority = 0.9
+
 
     def items(self):
         return ResourceURL.objects.all()
 
 class CourseSitemap(Sitemap):
-    changefreq = "daily"
-    priority = 0.9
+
 
     def items(self):
         return Course.objects.all()
 
 class DeptSemSitemap(Sitemap):
-    changefreq = "daily"
-    priority = 0.9
 
     def items(self):
         return DeptSem.objects.all()
